7fa4bf71f20f04c61301627ad0d11741c146f24a
CraxorAMXX <andrei.fuffy97@gmail.com>
2017-06-03 14:08:51 +0300

    Update Not even beta.cpp

--- Not even beta.cpp
+++ Not even beta.cpp
@@ -4,10 +4,9 @@
 #include <engine>
 #include <fun>
 #include <hamsandwich>
-#include <debug>
 
 new const PLUGIN_NAME[] = "Ghosts vs Hunters";
-new const VERSION_NUM[] = "1.0.0";
+new const VERSION_NUM[] = "b1.0";
 new const AUTHOR_NAME[] = "Craxor";
 
 
@@ -16,6 +15,7 @@ new const PlayerClass[] = "player";
 
 #define is_user_ghost(%0)	( cs_get_user_team(%0) == CS_TEAM_T )
 const ButtonBits = ( IN_FORWARD | IN_BACK | IN_MOVELEFT | IN_MOVERIGHT );
+new Ham:Ham_Player_ResetMaxSpeed = Ham_Item_PreFrame
 
 public plugin_init( )
 {
@@ -27,16 +27,71 @@ public plugin_init( )
 	);
 
 	RegisterHam( Ham_Spawn, PlayerClass, "ham_Spawn_EV", 1 );
+	RegisterHam(Ham_Player_ResetMaxSpeed,"player","playerResetMaxSpeed", 1)
+
 	register_forward( FM_CmdStart , "fw_FMCmdStart" );
 
-	register_clcmd( "say /test", "test" );
+	register_touch( "weaponbox", PlayerClass, "no_ghost_pickup" );
+	register_touch( "armoury_entity", PlayerClass, "no_ghost_pickup" );
+
+	register_event( "DeathMsg" , "func_deathmsg", "b", "1!0", "4!word" );
 }
 
-public test( id )
+public plugin_precache() 
 { 
-	debug_log( 1, "Test called" );
-	set_user_godmode(id, 1 );
-	debug_log( 1,"Is user godmode? %i", get_user_godmode(id) );
+	new Entity = create_entity( "info_map_parameters" );
+        
+	DispatchKeyValue( Entity, "buying", "3" );
+	DispatchSpawn( Entity );
+}
+
+public playerResetMaxSpeed(id)
+{
+       	if( is_user_alive(id) && is_user_ghost(id) )
+	{
+		set_user_maxspeed( id, 500.0 );
+	}
+} 
+
+public func_deathmsg()
+{
+	new iAtk = read_data(1);
+	new iVct = read_data(2);
+
+	if( iAtk != iVct && is_user_alive( iAtk ) && !is_user_ghost(iAtk) )
+	{
+		new AtkName[32], VctName[32];
+
+		get_user_name( iAtk, AtkName, charsmax(AtkName) );
+		get_user_name( iVct, VctName, charsmax(VctName) );
+
+		client_print( 0 , print_chat, "The hunter %s killed the ghost %s.", AtkName, VctName );
+	}
+}
+
+public no_ghost_pickup(ent, id)
+{
+	if( !pev_valid(ent) || !id )
+		return -1;
+
+	if( is_user_ghost(id) )
+	{
+		return PLUGIN_HANDLED;
+	}
+	return PLUGIN_CONTINUE;
+}
+public pfn_keyvalue( Entity )  
+{ 
+	new ClassName[ 20 ], Dummy[ 2 ];
+	copy_keyvalue( ClassName, charsmax( ClassName ), Dummy, charsmax( Dummy ), Dummy, charsmax( Dummy ) );
+        
+	if( equal( ClassName, "info_map_parameters" ) ) 
+	{ 
+		remove_entity( Entity );
+		return PLUGIN_HANDLED ;
+	} 
+        
+	return PLUGIN_CONTINUE;
 }
 
 public ham_Spawn_EV( id )
@@ -46,14 +101,15 @@ public ham_Spawn_EV( id )
 		strip_user_weapons( id );
 		give_item( id, "weapon_knife" );
 
-		set_user_godmode( id, is_user_ghost(id) ? 1 : 0 );
-		set_user_health( id, is_user_ghost(id) ? 10 : 125 );
+		set_user_noclip( id, is_user_ghost(id) ? 1 : 0 );
+		set_user_health(id, is_user_ghost(id) ? 10 : 125 );
+		set_user_maxspeed( id, is_user_ghost(id) ? 500.0 : 250.0 );
 
 		if( !is_user_ghost(id) )
 		{
 			give_user_weapon( id, CSW_USP, 12, 400 );
 			give_user_weapon( id, CSW_M4A1, 30, 800 );
-			give_user_weapon( id, CSW_HEGRENADE, 2 );
+			give_user_weapon( id, CSW_HEGRENADE, 1 );
 		}
 	}
 }
@@ -107,4 +163,4 @@ give_user_weapon( index , iWeaponTypeID , iClip=0 , iBPAmmo=0 , szWeapon[]="" , 
 	}
 	
 	return iWeaponEntity;
-}
+}		
